7ea24203919bc68af481b7031ab4ed51ae2958a6
Maciek <msitkowski94@gmail.com>
2021-11-03 07:42:07 +0100

    chore: update rollbar env variable

--- src/rollbar.tsx
+++ src/rollbar.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { ErrorBoundary, Provider } from '@rollbar/react';
+import Rollbar from 'rollbar';
 
-// same configuration you would create for the Rollbar.js SDK
-const rollbarConfig = {
-  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+const rollbarConfig: Rollbar.Configuration = {
+  accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
   environment: 'production',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
 };
 
 interface RollbarProviderProps {
