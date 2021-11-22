30d6ec2b3fe7d31b6ba6e920829fc8839d492b8d
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2021-11-22 18:40:43 +0100

    chore: disable Rollbar in development

--- src/rollbar.tsx
+++ src/rollbar.tsx
@@ -3,6 +3,7 @@ import { ErrorBoundary, Provider } from '@rollbar/react';
 import Rollbar from 'rollbar';
 
 const rollbarConfig: Rollbar.Configuration = {
+  enabled: process.env.NODE_ENV === 'production',
   accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
   environment: 'production',
 };
